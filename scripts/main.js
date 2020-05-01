009995d23b41d3a3f21069e0fb50e655f75fc390
J. Sierra <menelcar_0@hotmail.com>
2020-05-01 16:48:17 +0200

    pffffffff

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,28 @@
 
+var headerHeight = 0;
 
-//// TODO: Put all on vanilla js or all on jquery!!
-
-var stickyOffset = $('.sticky-nav').offset().top;
-
-$(window).scroll(function(){
-  var sticky = $('.sticky-nav'), scroll = $(window).scrollTop()
-
-  if (scroll >= stickyOffset)
-    sticky.addClass('top')
-  else
-    sticky.removeClass('top')
-})
-
-window.addEventListener('load', function(){
-  var allimages= document.getElementsByTagName('img');
-  for (var i=0; i<allimages.length; i++) {
-    if (allimages[i].getAttribute('data-src')) {
-      allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
-    }
-  }
-}, false)
-
-function showImage(element) {
-  document.getElementById("imageModal").src = element.src;
-  document.getElementById("imageModalWindow").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
+function updateMargin() {
+    headerHeight = document.getElementsByTagName("HEADER")[0].scrollHeight
+    document.getElementsByTagName("MAIN")[0].style.marginTop =  headerHeight+"px"
 }
+
+  document.getElementsByTagName("BODY")[0].onresize = updateMargin
+  
+  updateMargin()
+
+  function scrollEvent() {
+    let title = document.getElementById('title')
+
+    console.log(document.documentElement.scrollTop)
+
+    if (document.documentElement.scrollTop > 1) {
+      title.classList.add("smaller");
+    } else {
+      title.classList.remove("smaller");
+    }
+
+    setTimeout(updateMargin(), 1001);
+    
+}
+
+window.addEventListener('scroll', scrollEvent)
